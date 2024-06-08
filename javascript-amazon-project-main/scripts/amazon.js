672b36b06cbeb5775ee95c97a752345723f1fb85
Jay-Quan <jayquanmccleary1@gmail.com>
2024-06-08 13:10:29 -0500

    Use products.js file

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,28 +1,4 @@
-const products = [
-    
-    {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {Stars: "4.5", count: 87},
-    priceCents: "1090",
-    ID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
-},
 
-{
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {Stars: "4", count: 127},
-    priceCents: "2095",
-    ID: "15b6fc6f-327a-4ec4-896f-486349e85a3d"
-},
-
-{
-    image: "images/products/cotton-bath-towels-teal.webp",
-    name: "100% Cotton Bath Towels - 2 Pack, Light Teal",
-    rating: {Stars: "4.5", count: 56},
-    priceCents: "799",
-    ID: "8a53b080-6d40-4a65-ab26-b24ecf700bce"
-}];
 
 let productsHtml = "";
 

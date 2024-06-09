b45e50bdb9f8760140f550205b4167f543de72af
Jay-Quan <jayquanmccleary1@gmail.com>
2024-06-08 22:09:00 -0500

    added module

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-
+import { cart } from "../data/cart.js";
 
 let productsHtml = "";
 
@@ -78,6 +78,16 @@ document.querySelectorAll(`.js-add-to-cart`)
         quantity: 1
       });
     }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(`.js-cart-quantity`).innerHTML = cartQuantity;
+
+    console.log(cartQuantity);
 		console.log(cart);
 	});
 })

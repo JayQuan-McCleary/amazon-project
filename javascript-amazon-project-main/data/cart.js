48ebaaa1a35d9277cf34c333accb2c4ec616c432
Jay-Quan <jayquanmccleary1@gmail.com>
2024-06-09 11:09:40 -0500

    save cart in Local Storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,14 +1,21 @@
-export let cart = [
-{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
+export let cart = JSON.parse(localStorage.getItem("cart"));
 
-},
-{
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-}];
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2
+    
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1
+    }];
+}
 
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 export function addToCart(productId) {
     let matchingItem = "";
   
@@ -26,6 +33,8 @@ export function addToCart(productId) {
         quantity: 1
       });
     }
+
+    saveToStorage();
   }
   
   export function removeFromCart(productId) {
@@ -37,5 +46,6 @@ export function addToCart(productId) {
       }
     });
     cart = newCart;
+    saveToStorage();
 
   }

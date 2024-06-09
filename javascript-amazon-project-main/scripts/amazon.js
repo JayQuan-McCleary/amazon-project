822087c511ad9c4c8700e451023844e906dc08bb
Jay-Quan <jayquanmccleary1@gmail.com>
2024-06-08 22:52:34 -0500

    Generate html for order summary

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHtml = "";
@@ -58,37 +58,22 @@ products.forEach((product) => {
 document.querySelector(".js-products-grid").
 innerHTML = productsHtml;
 
-document.querySelectorAll(`.js-add-to-cart`)
-.forEach((button) => {
-	button.addEventListener(`click`, () => {
-		const productId = button.dataset.
-    productId;
-    let matchingItem = "";
+function updateCartQuantity() {
+  let cartQuantity = 0;
 
-    cart.forEach((item) => {
-      if ( productId === item.productId ) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
     });
 
     document.querySelector(`.js-cart-quantity`).innerHTML = cartQuantity;
+}
+document.querySelectorAll(`.js-add-to-cart`)
+.forEach((button) => {
+	button.addEventListener(`click`, () => {
+    const productId = button.dataset.
+    productId;
 
-    console.log(cartQuantity);
-		console.log(cart);
+    addToCart(productId);
+    updateCartQuantity();
 	});
 })
